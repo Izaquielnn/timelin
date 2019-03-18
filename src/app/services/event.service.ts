950ce71bfd945d789a2b8a398f26a4b9a65ff67f
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-18 19:59:56 -0300

    add event services

---
+++ src/app/services/event.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Event } from '../models/event.model';
+import { Tag } from '../models/tag.model';
+import  BASE_URL  from '../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  constructor(private http: HttpClient) {}
+
+  getEvents(): Observable<any>{
+    const url = `${BASE_URL}/events`;
+    return this.http.get(url);
+  }
+
+  addEvent(name: string, description: string, tags: Tag[], color: string): Observable<any> {
+    const url = `${BASE_URL}/tags`;
+    return this.http.post<Event>(url, {name, description, color, tags});
+  }
+
+  removeEvent(id: number): Observable<any>{
+    const url = `${BASE_URL}/events/${id}`;
+    return this.http.delete(url);
+  }
+
+  updateEvent(event: Event): Observable<any>{
+    const url = `${BASE_URL}/tags/${event.id}`;
+    return this.http.put(url, event);
+  }
+}
