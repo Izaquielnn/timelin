11332735f40512186c1d2a8ecebe075dd01b075c
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-18 19:38:17 -0300

    add token interceptor for requests

---
+++ src/app/services/token.interceptor.ts
@@ -0,0 +1,49 @@
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { Router } from '@angular/router';
+import { 
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler, 
+  HttpRequest,
+  HttpErrorResponse 
+} from '@angular/common/http';
+
+import { AuthService } from './auth.service';
+
+
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  private authService: AuthService;
+  constructor(private injector: Injector) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.authService = this.injector.get(AuthService);
+    const token: string = this.authService.getToken();
+    request = request.clone({
+      setHeaders: {
+        'x-auth-token': token,
+        'Content-Type': 'application/json'
+      }
+    });
+    return next.handle(request);
+  }
+}
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    return next.handle(request)
+      .catch((response: any) => {
+        if (response instanceof HttpErrorResponse && response.status === 401) {
+          localStorage.removeItem('token');
+          this.router.navigateByUrl('/login');
+        }
+        return Observable.throw(response);
+      });
+  }
+}
