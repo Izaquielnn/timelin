84d4d3aabaaf7469eda0baf36097ee9762c1030e
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-18 19:44:14 -0300

    add signup component

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { User } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  user: User = new User();
+  errorMessage: string | null;
+
+  constructor( private authService: AuthService,  private router: Router) {
+  }
+
+  ngOnInit() {
+  };
+
+  onSubmit(): void {
+    this.authService.signUp(this.user.name,this.user.email, this.user.password)
+    .subscribe(
+      () => {
+        this.router.navigateByUrl('/')
+      },
+      error => this.errorMessage = error.error
+    )
+  }
+}
