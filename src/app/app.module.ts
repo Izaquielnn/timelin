90c798c68a3bee93b91b6b2d298fcd1f0cbf49a8
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-19 07:54:56 -0300

    add timeline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
@@ -12,12 +12,17 @@ import { SignupComponent } from './components/signup/signup.component';
 import { AuthGuardService as AuthGuard, AuthGuardService} from './services/auth-guard.service';
 import { reducer as eventReducer}  from './store/reducers/event.reducers';
 import { reducer as TagReducer}  from './store/reducers/tag.reducers';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { TokenInterceptor, ErrorInterceptor } from './services/token.interceptor';
+import { TimelineComponent } from './components/timeline/timeline.component';
  
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
+    HomepageComponent,
+    TimelineComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +31,8 @@ import { reducer as TagReducer}  from './store/reducers/tag.reducers';
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent, canActivate: [AuthGuard] },
-      
+      { path: 'timelin', component: TimelineComponent, canActivate: [AuthGuard] },
+      { path: '', component: HomepageComponent },
       { path: '**', redirectTo: '/' }
     ]),
     StoreModule.forRoot({
@@ -34,7 +40,20 @@ import { reducer as TagReducer}  from './store/reducers/tag.reducers';
       tag: TagReducer
     })
   ],
-  providers: [AuthService, AuthGuardService],
+  providers: [
+    AuthService, 
+    AuthGuardService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
